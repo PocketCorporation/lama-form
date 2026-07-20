fe79c75373161ea29b2400e575b0cc8dfaf17358
PocketCorporation <73258409+PocketCorporation@users.noreply.github.com>
2026-07-20 15:58:37 +0000

    validation complete

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './formInput.css'
 
 const FormInput = (props) => {
     const {label,errorMessage,onChange,id,...inputProps}=props
+    const [focus,setFocus]=useState(false)
+
+    const handleFocus=(e)=>{
+      setFocus(true)
+    }
   return (
     <div className='formInput'>
       <label>{label}</label>
       <input
         {...inputProps}
         onChange={onChange}
+        onBlur={handleFocus}
+        focus={focus.toString()}
       />
       <span>{errorMessage}</span>
     </div>
